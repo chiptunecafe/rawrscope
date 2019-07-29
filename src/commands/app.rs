73b4b89850c13340a25bc82d6511109a5557a8a7
Max Beck <rytonemail@gmail.com>
2019-07-29 16:56:38 -0500

    refactor main app to be more readable

--- src/commands/app.rs
+++ src/commands/app.rs
@@ -2,18 +2,25 @@ use std::io;
 use std::panic::{set_hook, take_hook};
 
 use cpal::traits::{DeviceTrait, HostTrait};
+use snafu::{OptionExt, ResultExt, Snafu};
 
 use crate::audio::{connection::ConnectionTarget, mixer, playback};
 use crate::config;
 use crate::panic;
 use crate::state::{self, State};
 
-pub fn run(state_file: Option<&str>) {
-    set_hook(panic::dialog(take_hook()));
+// Errors are usually used when the app should quit
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("No output device available on host \"{:?}\"", host))]
+    NoOutputDevice { host: cpal::HostId },
 
-    let config = config::Config::load();
+    #[snafu(display("Failed to create master audio player: {}", source))]
+    MasterCreation { source: playback::CreateError },
+}
 
-    let mut state = match state_file {
+fn load_state(state_file: Option<&str>) -> state::State {
+    match state_file {
         Some(path) => match State::from_file(path) {
             Ok((state, warnings)) => {
                 for w in warnings {
@@ -28,9 +35,10 @@ pub fn run(state_file: Option<&str>) {
                 log::warn!("Project not found, writing default...");
                 let state = State::default();
                 if let Err(e) = state.write(path) {
-                    log::error!("Failed to write new project: {}", e);
+                    log::warn!("Failed to write new project: {}", e);
+                } else {
+                    log::debug!("Created new project at {}", path);
                 }
-                log::debug!("Created new project at {}", path);
                 state
             }
             Err(e) => {
@@ -39,14 +47,16 @@ pub fn run(state_file: Option<&str>) {
             }
         },
         None => State::default(),
-    };
+    }
+}
 
-    let audio_host = match config.audio.host {
+fn audio_host(config: &config::Config) -> cpal::Host {
+    match &config.audio.host {
         Some(host_name) => {
             if let Some((id, _n)) = cpal::available_hosts()
                 .iter()
                 .map(|host_id| (host_id, format!("{:?}", host_id)))
-                .find(|(_id, n)| n == &host_name)
+                .find(|(_id, n)| n == host_name)
             {
                 cpal::host_from_id(*id).unwrap_or_else(|err| {
                     log::warn!(
@@ -62,76 +72,81 @@ pub fn run(state_file: Option<&str>) {
             }
         }
         None => cpal::default_host(),
-    };
-    let audio_dev = match config.audio.device {
-        Some(dev_name) => match audio_host.output_devices() {
+    }
+}
+
+fn audio_device(config: &config::Config, host: &cpal::Host) -> Result<cpal::Device, Error> {
+    match &config.audio.device {
+        Some(dev_name) => match host.output_devices() {
             Ok(mut iter) => match iter.find(|dev| {
                 dev.name()
                     .ok()
-                    .map(|name| name == dev_name)
+                    .map(|name| &name == dev_name)
                     .unwrap_or(false)
             }) {
-                Some(d) => d,
+                Some(d) => Ok(d),
                 None => {
-                    log::error!("Output device \"{}\" does not exist!", dev_name);
-                    return;
+                    log::warn!(
+                        "Output device \"{}\" does not exist ... using default",
+                        dev_name
+                    );
+                    host.default_output_device()
+                        .context(NoOutputDevice { host: host.id() })
                 }
             },
             Err(e) => {
-                log::error!("Failed to query output devices: {}", e);
-                return;
+                log::warn!(
+                    "Failed to query output devices: {} ... attempting to use default",
+                    e
+                );
+                host.default_output_device()
+                    .context(NoOutputDevice { host: host.id() })
             }
         },
-        None => match audio_host.default_output_device() {
-            Some(d) => d,
-            None => {
-                log::error!("No output device available!");
-                return;
-            }
-        },
-    };
+        None => host
+            .default_output_device()
+            .context(NoOutputDevice { host: host.id() }),
+    }
+}
 
-    let mut master = match playback::Player::new(audio_host, audio_dev) {
-        Ok(p) => p,
-        Err(e) => {
-            log::error!("{}", e);
-            return;
-        }
-    };
+fn _run(state_file: Option<&str>) -> Result<(), Error> {
+    set_hook(panic::dialog(take_hook()));
+
+    let config = config::Config::load();
+    let mut state = load_state(state_file);
+
+    let audio_host = audio_host(&config);
+    let audio_dev = audio_device(&config, &audio_host)?;
+
+    let mut master = playback::Player::new(audio_host, audio_dev).context(MasterCreation)?;
 
     let mut mixer_config = mixer::MixerBuilder::new();
     mixer_config.channels(master.channels() as usize);
     mixer_config.target_sample_rate(master.sample_rate());
 
     for source in state.audio_sources.iter_mut().filter_map(|s| s.as_loaded()) {
-        let channels = source.spec().channels;
         let sample_rate = source.spec().sample_rate;
-        let len = source.len();
-
         mixer_config.source_rate(sample_rate);
-
-        let time_secs = (len / u32::from(channels)) as f32 / sample_rate as f32;
-        println!("{}: {:.2}s", source.path().display(), time_secs);
     }
 
     if let Err(e) = master.rebuild_mixer(mixer_config) {
         log::warn!("Failed to rebuild master mixer: {}", e);
     }
 
-    let sub_builder = master.submission_builder();
-
     let framerate = 60u16;
     let frame_secs = 1.0 / f32::from(framerate);
 
-    let window_ms = 200;
+    let window_ms = 50;
 
     let frame_len = std::time::Duration::from_micros(1_000_000 / u64::from(framerate));
+
     let mut loaded_sources = state
         .audio_sources
         .iter_mut()
         .filter_map(|s| s.as_loaded())
         .collect::<Vec<_>>();
 
+    let sub_builder = master.submission_builder();
     let mut frame = 0;
     loop {
         let st = std::time::Instant::now();
@@ -144,8 +159,8 @@ pub fn run(state_file: Option<&str>) {
 
             let window_len = sample_rate * window_ms / 1000 * u32::from(channels);
             let window_pos = (sample_rate / u32::from(framerate)) * frame;
-            // TODO dont panic
 
+            // TODO dont panic
             let window = source
                 .chunk_at(window_pos, window_len as usize)
                 .unwrap()
@@ -183,5 +198,11 @@ pub fn run(state_file: Option<&str>) {
         std::thread::sleep((frame_len - elapsed) / 2);
 
         frame += 1;
+    }
+}
+
+pub fn run(state_file: Option<&str>) {
+    if let Err(e) = _run(state_file) {
+        log::error!("{}", e)
     }
 }
