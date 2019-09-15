2bffac19c253efcab35445927431f12de717eb32
rytone <rytonemail@gmail.com>
2019-09-15 15:58:30 -0500

    do not store audio buffer in scope struct
    processing should be done over immutable views of the audio to
    prevent copying memory

--- src/scope.rs
+++ src/scope.rs
@@ -1,13 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-use crate::audio::mixer;
-
 #[derive(Serialize, Deserialize)]
 pub struct Scope {
     pub channels: i16,
     pub window_size: f32,
-    #[serde(skip)]
-    pub buffer: Vec<f32>,
 }
 
 impl Scope {
