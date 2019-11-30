b83c1bca0f5c1292889c2131ccaab3939e3742c1
Max Beck <rytonemail@gmail.com>
2019-11-29 21:18:00 -0600

    add basic playback controls to ui

--- src/ui.rs
+++ src/ui.rs
@@ -8,5 +8,16 @@ pub fn ui<'a, 'ui>(state: &'a mut State, ui: &'a Ui<'ui>) {
         .build(&ui, || {
             ui.text(im_str!("Playing: {}", state.playback.playing));
             ui.text(im_str!("Frame: {}", state.playback.frame));
+            if ui.small_button(im_str!("Play/Pause")) {
+                state.playback.playing = !state.playback.playing;
+            }
+            ui.same_line(0.0);
+            if ui.small_button(im_str!("+100 frames")) {
+                state.playback.frame = state.playback.frame.saturating_add(100);
+            }
+            ui.same_line(0.0);
+            if ui.small_button(im_str!("-100 frames")) {
+                state.playback.frame = state.playback.frame.saturating_sub(100);
+            }
         });
 }
