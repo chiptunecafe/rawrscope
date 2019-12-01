daa911d6e25c20c39223e9136f33266c551f0759
Max Beck <rytonemail@gmail.com>
2019-12-01 00:21:11 -0600

    read and store config in system-level config dir

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{self, Read, Write};
 
 use serde::{Deserialize, Serialize};
-use snafu::{ResultExt, Snafu};
+use snafu::{OptionExt, ResultExt, Snafu};
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct Audio {
@@ -17,8 +17,14 @@ pub struct Config {
 
 #[derive(Debug, Snafu)]
 pub enum Error {
+    #[snafu(display("Could not find a home directory!"))]
+    HomeDirectory,
+
     #[snafu(display("Failed to create config file: {}", source))]
     CreateError { source: io::Error },
+
+    #[snafu(display("Failed to create config directory: {}", source))]
+    CreateDirectory { source: io::Error },
 
     #[snafu(display("Failed to serialize config: {}", source))]
     SerializeError { source: toml::ser::Error },
@@ -27,10 +33,24 @@ pub enum Error {
     WriteError { source: io::Error },
 }
 
-// TODO do not store config in working dir
 impl Config {
+    fn config_dir() -> Result<directories::ProjectDirs, Error> {
+        directories::ProjectDirs::from("", "rytone", "rawrscope").context(HomeDirectory)
+    }
+
     pub fn load() -> Self {
-        let mut file = match fs::File::open("rawrscope.toml") {
+        let mut path = match Config::config_dir() {
+            Ok(p) => p,
+            Err(_) => {
+                log::warn!("No suitable home directory found! Using default config...");
+                return Default::default();
+            }
+        }
+        .config_dir()
+        .to_path_buf();
+        path.push("rawrscope.toml");
+
+        let mut file = match fs::File::open(path) {
             Ok(f) => f,
             Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
                 log::debug!("No config found... using default");
@@ -58,7 +78,17 @@ impl Config {
     }
 
     pub fn write(&self) -> Result<(), Error> {
-        let mut file = fs::File::create("rawrscope.toml").context(CreateError)?;
+        let dir = Config::config_dir()?;
+
+        fs::DirBuilder::new()
+            .recursive(true)
+            .create(dir.config_dir())
+            .context(CreateDirectory)?;
+
+        let mut path = dir.config_dir().to_path_buf();
+        path.push("rawrscope.toml");
+
+        let mut file = fs::File::create(path).context(CreateError)?;
         let serialized = toml::to_string_pretty(self).context(SerializeError)?;
         file.write_all(serialized.as_ref()).context(WriteError)
     }
