eba463d1e90fe26cce8ce281bf79ece3934c9710
Max Beck <rytonemail@gmail.com>
2019-12-01 19:59:33 -0600

    add git version to ui
    should help with support while the actual version is stuck at 0.1.0

--- src/ui.rs
+++ src/ui.rs
@@ -2,35 +2,39 @@ use crate::state::State;
 use imgui::{im_str, Ui};
 
 pub fn ui<'a, 'ui>(state: &'a mut State, ui: &'a Ui<'ui>) {
-    imgui::Window::new(im_str!("Rawrscope"))
-        .size([300.0, 120.0], imgui::Condition::Always)
-        .resizable(false)
-        .build(&ui, || {
-            // Status
-            ui.text(im_str!("Playing: {}", state.playback.playing));
-            ui.text(im_str!("Frame: {}", state.playback.frame));
+    imgui::Window::new(&im_str!(
+        "rawrscope {} ({})",
+        clap::crate_version!(),
+        git_version::git_version!()
+    ))
+    .size([300.0, 120.0], imgui::Condition::Always)
+    .resizable(false)
+    .build(&ui, || {
+        // Status
+        ui.text(im_str!("Playing: {}", state.playback.playing));
+        ui.text(im_str!("Frame: {}", state.playback.frame));
 
-            // Playback controls
-            if ui.small_button(im_str!("Play/Pause")) {
-                state.playback.playing = !state.playback.playing;
-            }
-            ui.same_line(0.0);
-            if ui.small_button(im_str!("+100 frames")) {
-                state.playback.frame = state.playback.frame.saturating_add(100);
-            }
-            ui.same_line(0.0);
-            if ui.small_button(im_str!("-100 frames")) {
-                state.playback.frame = state.playback.frame.saturating_sub(100);
-            }
+        // Playback controls
+        if ui.small_button(im_str!("Play/Pause")) {
+            state.playback.playing = !state.playback.playing;
+        }
+        ui.same_line(0.0);
+        if ui.small_button(im_str!("+100 frames")) {
+            state.playback.frame = state.playback.frame.saturating_add(100);
+        }
+        ui.same_line(0.0);
+        if ui.small_button(im_str!("-100 frames")) {
+            state.playback.frame = state.playback.frame.saturating_sub(100);
+        }
 
-            ui.separator();
+        ui.separator();
 
-            // Save project
-            if ui.small_button(im_str!("Save project")) {
-                // TODO do not panic
-                state
-                    .write(&state.file_path)
-                    .expect("could not save project");
-            }
-        });
+        // Save project
+        if ui.small_button(im_str!("Save project")) {
+            // TODO do not panic
+            state
+                .write(&state.file_path)
+                .expect("could not save project");
+        }
+    });
 }
