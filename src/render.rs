cef86965fa8e99d2c3a841cd0afe5c30122dea39
Max Beck <rytonemail@gmail.com>
2019-12-13 13:38:36 -0600

    add stub render method to renderer

--- src/render.rs
+++ src/render.rs
@@ -20,4 +20,22 @@ impl Renderer {
             }),
         }
     }
+
+    fn render(&self, encoder: &mut wgpu::CommandEncoder) {
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &self.tex.create_default_view(),
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: wgpu::Color {
+                    r: 0.0,
+                    g: 0.0,
+                    b: 0.0,
+                    a: 1.0,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+    }
 }
