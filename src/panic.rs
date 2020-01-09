9c482cb73d4757a6963d133d1ecef8c0a61e5332
Maxwell Beck <max@ryt.one>
2020-01-08 20:08:52 -0600

    fix error reporting in tfd panic handler
    should help debug #8

--- src/panic.rs
+++ src/panic.rs
@@ -8,15 +8,23 @@ pub fn dialog(old_handler: PanicHandler) -> PanicHandler {
     Box::new(move |info: &panic::PanicInfo| {
         old_handler(info);
 
-        let info = if let Some(info) = info.payload().downcast_ref::<&str>() {
-            info
-        } else {
-            "No additional information."
+        // should always exist according to default panic hook comments
+        let location = info.location().unwrap();
+
+        let info = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &s[..],
+                None => "No information available...",
+            },
         };
 
         tfd::message_box_ok(
             "Oops!",
-            &format!("rawrscope encontered an unrecoverable error!\n{}", info),
+            &format!(
+                "rawrscope encontered an unrecoverable error!\n{}\n(at {})",
+                info, location
+            ),
             tfd::MessageBoxIcon::Error,
         );
     })
